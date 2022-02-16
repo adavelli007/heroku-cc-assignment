8e2e8593e391ec9ca4c5deb8b3299fb9bacffb1a
adavelli007 <99699527+adavelli007@users.noreply.github.com>
2022-02-15 20:48:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
   <body>
       <div">
       <h3 style="color:red"><b>"Simple Static Website Assignment Spring 2022 using Cloud Managed Web Service"</b></h3>
-        <h5>Name: Amarendhar Reddy Adavelli</h5>
-        <h5>Student ID: 16322277</h5>
-        <h5>Cloud Provider: Heroku</h5>
+        <h4>Name: Amarendhar Reddy Adavelli</h4>
+        <h4>Student ID: 16322277</h4>
+        <h4>Cloud Provider: Heroku</h4>
 
       </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
